000f69645b1b95cc98dd8b1e3923f480b63bddc4
Tennessine <389443626@qq.com>
2020-01-25 03:27:42 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Symfony\Component\Process\Process;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::post('/deploy', function () {
+	$root_path = base_path();
+	$process = new Process('cd ' . $root_path . '; ./deploy.sh');
+	$process->run(function ($type, $buffer) {
+		echo $buffer;
+	});
 });
