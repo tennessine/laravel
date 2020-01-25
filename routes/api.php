c931d14e63233f47a5283399fef1b7da0576db53
Tennessine <389443626@qq.com>
2020-01-25 17:49:38 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,8 @@ use Illuminate\Http\Request;
 
 Route::post('/upload', function (Request $request) {
 	$file = $request->file('file');
-	return $file->getClientOriginalName();
+	// wx9272a2c1b71081f4.o6zAJszrdmwFQCryome-HIFrEQaI.cFCK7ArKXwFm0e65e9c559c051ee27aaa2c23730b825.png
+	// $file->getClientOriginalName();
+	//
+	return $file->getClientOriginalExtension();
 });
