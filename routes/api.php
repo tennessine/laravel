344ac8c4fbf3186d78bba6887c77f4e991ec63e6
Tennessine <389443626@qq.com>
2020-01-25 18:03:11 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,12 @@ Route::post('/upload', function (Request $request) {
 	$file = $request->file('file');
 	// wx9272a2c1b71081f4.o6zAJszrdmwFQCryome-HIFrEQaI.cFCK7ArKXwFm0e65e9c559c051ee27aaa2c23730b825.png
 	// $file->getClientOriginalName();
+
 	// png
 	// return $file->getClientOriginalExtension();
-	//
-	return $file->getRealPath();
+
+	// /tmp/phpBoaM13
+	// return $file->getRealPath();
+
+	return $file->getSize();
 });
